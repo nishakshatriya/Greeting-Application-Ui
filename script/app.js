84d4cf4349cf934b3c2db16d998f201d5f01f93f
nishakshatriya <nisha.kshatriya08@gmail.com>
2020-09-10 14:17:29 +0530

    Added: Popup box on click of Add operation

---
+++ script/app.js
@@ -0,0 +1,39 @@
+document.querySelector(".add-side-navbar").addEventListener("click", () => {
+    showPopup(".add-popup");
+  });
+  
+  function showPopup(classname) {
+    let popuplist = [".add-popup"];
+    popuplist.forEach(function (clsname) {
+      if (clsname !== classname) {
+        document.querySelector(clsname).style.display = "none";
+      }
+    });
+
+    let popup = document.querySelector(classname);
+    let displayStatus = popup.style.display;
+    if (displayStatus !== "none" || displayStatus !== "") {
+      popup.style.display = "none";
+      document.getElementsByClassName("popup-boxes")[0].style.display ="none"
+    }
+    if (displayStatus === "none" || displayStatus === "") {
+      popup.style.display = "block";
+      document.getElementsByClassName("popup-boxes")[0].style.display ="block"
+    }
+  }
+
+  let closePopup = document.querySelectorAll(".close-button");
+  closePopup.forEach((closeElement) => {
+    closeElement.addEventListener("click", () => {
+      document.getElementsByClassName("popup-boxes")[0].style.display ="none"
+      ClosePopUp();
+    });
+  });
+  
+  function ClosePopUp() {
+    let popuplist = [".add-popup"];
+    popuplist.forEach(function (clsname) {
+      document.querySelector(clsname).style.display = "none";
+    });
+  }
+  
