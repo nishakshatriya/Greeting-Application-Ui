83f3913dae87686ce4d424cfb41e7957fc003009
nishakshatriya <nisha.kshatriya08@gmail.com>
2020-09-14 12:23:49 +0530

    Added: Integrate and get all greeting from db

--- script/app.js
+++ script/app.js
@@ -1,10 +1,17 @@
+const baseURL = "http://localhost:3000/greetings/"
 
-// $.ajax('http://localhost:3000/greetings/',
-// {
-//   success: function(data,status,xhr){
-//     $('msgs-list').append(data);
-//   }
-// })
+function getCards(){
+  fetch(`${baseURL}`)
+  .then(data =>{
+    return data.json()
+  })
+  .then(res => {
+    displayCards(res)
+  })
+  .catch(err => console.log(err))
+}
+
+getCards()
 
 document.querySelector(".add-side-navbar").addEventListener("click", () => {
     showPopup(".add-popup");
@@ -53,45 +60,37 @@ document.querySelector(".add-side-navbar").addEventListener("click", () => {
     });
   }
   
-list = [{
-    "id":"QWersdf1234rvboiQ",
-    "name":"Nisha",
-    "message":"Hello Nisha",
-    "time": "5 min ago"
-},
-{
-    "id":"QWersdf1234rvboiQ",
-    "name":"Nisha",
-    "message":"Hello Nisha",
-    "time": "5 min ago"
-},
-{   "id":"QWersdf1234rvboiQ",
-    "name":"Nisha",
-    "message":"Hello Nisha",
-    "time": "5 min ago"
-}]
-
-function displayCards(){
+function displayCards(list){
   let listOfContent = document.querySelector(".messages-list");
-  // let box = document.createElement("div");
+  let box = document.createElement("div");
   for(let i = 0; i< list.length; i++){
       let messageContent = document.createElement("div");
       messageContent.className = "message-content-box";
+      
       let name = document.createElement("div");
       name.className = "name-div";
-      name.innerText = list[i].name;
+      name.innerText = list[i].firstName;
+
+      let lname = document.createElement("div");
+      lname.className = "name-div";
+      lname.innerText = list[i].lastName;
+      
       let message = document.createElement("div");
       message.className = "message-div";
       message.innerText = list[i].message;
+      
       let time = document.createElement("div");
       time.className = "time-div";
-      time.innerText = list[i].time; 
+      time.innerText = list[i].createdAt; 
+      
       let id = document.createElement("div");
-      id.className = "time-div";
-      id.innerText = list[i].id;
+      id.className = "id-div";
+      id.innerText = list[i]._id;
+      id.innerHTML = "ObjectId("+list[i]._id+")";
 
       messageContent.appendChild(id)
       messageContent.appendChild(name);
+      messageContent.appendChild(lname);
       messageContent.appendChild(message);
       messageContent.appendChild(time);
       listOfContent.appendChild(messageContent);
